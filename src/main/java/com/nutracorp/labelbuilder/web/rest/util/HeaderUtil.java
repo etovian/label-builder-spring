9426a43fc68dc2877bed33fa25d3b675750ceb57
Mike Green <mike.green@stgconsulting.com>
2016-01-29 10:48:50 -0700

    Changed the phrasing for alerts.

--- src/main/java/com/nutracorp/labelbuilder/web/rest/util/HeaderUtil.java
+++ src/main/java/com/nutracorp/labelbuilder/web/rest/util/HeaderUtil.java
@@ -16,15 +16,15 @@ public class HeaderUtil {
     }
 
     public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
-        return createAlert("A new " + entityName + " is created with identifier " + param, param);
+        return createAlert("A new " + entityName + " was created with identifier " + param, param);
     }
 
     public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is updated with identifier " + param, param);
+        return createAlert("A " + entityName + " was updated with identifier " + param, param);
     }
 
     public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
-        return createAlert("A " + entityName + " is deleted with identifier " + param, param);
+        return createAlert("The following " + entityName + " was deleted: " + param, param);
     }
 
     public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
