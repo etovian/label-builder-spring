7a3ab121dc72e8e63f7c79b69081d0cbc968af5c
Mike Green <mike.green@stgconsulting.com>
2016-02-05 15:06:37 -0700

    Removed the .load method, which was never called.  The label entity is retrieved as part of a route-resolved promise.

--- src/main/webapp/scripts/app/entities/label/label-detail.controller.js
+++ src/main/webapp/scripts/app/entities/label/label-detail.controller.js
@@ -8,12 +8,7 @@ angular.module('labelbuilderApp')
     .controller('LabelDetailController', function ($scope, $rootScope, $stateParams, entity, Label) {
 
         var vm = angular.extend(this, {
-            label: entity,
-            load: function() {
-                Label.get({id: id}, function(result) {
-                    vm.label = result;
-                });
-            }
+            label: entity
         });
 
         //TODO: not exactly sure why this is necessary, but removing it breaks a test
